cf874ab999322b4cd6fa7cf0568e991632ce3062
DevBloo <devchanghwan@gmail.com>
2021-07-10 14:29:08 +0900

    [#1] feat: 공통 기능 설계

---
+++ server/src/main/java/com/forrest/server/web/controller/ApiPostController.java
@@ -0,0 +1,15 @@
+package com.forrest.server.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Created by Bloo
+ * @Date: 2021/07/10
+ */
+
+
+@RestController
+public class ApiPostController {
+    // TODO: 2021/07/10  Post Controller 구현해주세요
+}
---
+++ server/src/main/java/com/forrest/server/web/domain/post/Post.java
@@ -0,0 +1,55 @@
+package com.forrest.server.web.domain.post;
+
+import com.forrest.server.web.domain.BaseTimeEntity;
+import com.forrest.server.web.domain.category.Category;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Created by Bloo
+ * @Date: 2021/07/10
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Post  extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false )
+    private int grade;
+
+    @Column(nullable = false)
+    private boolean open;
+
+    @ManyToOne ( fetch = FetchType.LAZY,
+                 cascade = CascadeType.ALL )
+    private Category category;
+
+    @Builder
+    public Post ( String title, String content, int grade, boolean open ) {
+        this.title = title;
+        this.content = content;
+        this.grade = grade;
+        this.open = open;
+    }
+
+}
